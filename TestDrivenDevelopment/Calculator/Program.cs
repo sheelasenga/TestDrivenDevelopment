0ee7464630540932c1a80dbc56ab279c0981ae7c
sheelasenga <54633529+sheelasenga@users.noreply.github.com>
2019-09-05 09:34:24 +0100

    changes

---
+++ TestDrivenDevelopment/Calculator/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using Calculator;
+
+namespace Calculator
+{
+    public class Program
+    {
+         static void Main(string[] args)
+        {
+        }
+
+    }
+}
+public class CalculatorunderTest
+{
+    public double Divide(double num1, double num2)
+    {
+        double result = num1 / num2;
+        return result;
+    }
+    public double Add(double num1, double num2)
+    {
+        double result1 = num1 + num2;
+        return result1;
+    }
+    public double Minus(double num1, double num2)
+    {
+        double result2 = num1 - num2;
+        return result2;
+    }
+    public double Multiply(double num1, double num2)
+    {
+        double result3 = num1 * num2;
+        return result3;
+    }
+}
